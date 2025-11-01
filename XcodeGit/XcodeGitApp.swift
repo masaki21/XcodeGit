1d598b6aac195a037f43c4fce2123e88b82084b6
masaki21 <omaemasaki21@kit.edu.kh>
2025-11-01 18:15:47 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by MAC on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
